d445c4501185daf711a7d0d608027f338816f876
supunkamburugamuve <supun06@gmail.com>
2016-08-16 09:08:58 -0400

    intial add

---
+++ heron/common/src/cpp/network/rdma/rdma_client.h
@@ -0,0 +1,6 @@
+#ifndef RDMA_CLIENT_H_
+#define RDMA_CLIENT_H_
+
+
+
+#endif /*RDMACLIENT_H_ */
---
+++ heron/common/src/cpp/network/rdma/rdma_event_loop.h
@@ -0,0 +1,6 @@
+#ifndef RDMA_EVENT_LOOP_H_
+#define RDMA_EVENT_LOOP_H_
+
+
+
+#endif /*RDMA_EVENT_LOOP_H_ */
